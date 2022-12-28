172caf6324796d375232ab379267894ab4376233
Stanislav <stas-chubar@yandex.ru>
2022-12-28 23:55:03 +0700

    minor update

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='lptransportsolver',
+    version='1.0.0',
+    packages=['lptransportsolver', 'lptransportsolver.Solver'],
+    url='https://github.com/schubarkrsk/LPTransportSolver',
+    license='GNU GPL-V3',
+    author='S. Chubar, A. Semenova',
+    author_email='stas-chubar@yandex.ru, asemenova@sfu-kras.ru',
+    description='This is small library with GUI in future which help solve transport problem in linear programming'
+)
